29a82d501fed0e39e646b5741a6550df58debd4c
SonOfMagic <qq1324318532@gmail.com>
2023-04-06 17:57:56 +0800

    chore: add doc link ref

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -7,6 +7,7 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['@typescript-eslint'],
+  // https://stackoverflow.com/questions/67437478/why-eslint-dont-see-global-typescript-types-in-vue-files-no-undef
   overrides: [
     {
       files: ['*.ts', '*.mts', '*.cts', '*.tsx'],
