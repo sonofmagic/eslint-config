ee05371fdf47cfee1f1368c8437bad86cb716b87
SonOfMagic <qq1324318532@gmail.com>
2023-04-06 18:07:28 +0800

    chore: commit typescript-eslint ref doc

--- packages/typescript/index.js
+++ packages/typescript/index.js
@@ -7,6 +7,7 @@ module.exports = {
     sourceType: 'module'
   },
   plugins: ['@typescript-eslint'],
+  // https://typescript-eslint.io/linting/troubleshooting#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
   // https://stackoverflow.com/questions/67437478/why-eslint-dont-see-global-typescript-types-in-vue-files-no-undef
   overrides: [
     {
