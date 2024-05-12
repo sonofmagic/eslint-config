28ce0cb64309e4221c4f0d8fcafedb968dc0d545
ice breaker <1324318532@qq.com>
2024-05-12 21:27:36 +0800

    chore: commit sxzz

--- packages/icebreaker/src/index.ts
+++ packages/icebreaker/src/index.ts
@@ -1,15 +1,2 @@
-import { sxzz } from '@sxzz/eslint-config'
-// eslint-disable-next-line import/no-named-as-default
-import antfu from '@antfu/eslint-config'
-
-// import defu from 'defu'
-
-type SxzzArgs = Parameters<typeof sxzz>
-
-export function icebreaker(...args: SxzzArgs) {
-  return sxzz(args[0], args[1])
-}
-
-export function icebreakerWithoutPrettier(...args: Parameters<typeof antfu>) {
-  return antfu(...args)
-}
+export { sxzz as icebreaker } from '@sxzz/eslint-config'
+export { default as icebreakerWithoutPrettier } from '@antfu/eslint-config'
