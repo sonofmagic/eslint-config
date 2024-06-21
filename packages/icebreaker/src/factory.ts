549e8e85fb9e89c4572d9fb17b89c5d80289f5bf
ice breaker <1324318532@qq.com>
2024-06-21 23:51:42 +0800

    chore: release 0.3.9

--- packages/icebreaker/src/factory.ts
+++ packages/icebreaker/src/factory.ts
@@ -28,7 +28,9 @@ export function icebreaker(
         'ts/prefer-ts-expect-error': 'off',
         'ts/ban-ts-comment': 'off',
         'vue/attribute-hyphenation': 'off',
-        'unused-imports/no-unused-imports': 'error',
+        // 问题在于 auto fix 的时候，会直接 remove 整个 import ，而我们想让用户自己去 remove
+        // 'unused-imports/no-unused-imports': 'error',
+        'no-unused-vars': 'warn',
       },
     },
   ]
