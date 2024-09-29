747f4a2cabaa8a8b43e568fa5397f45607a3f738
ice breaker <1324318532@qq.com>
2024-09-29 22:34:09 +0800

    chore: commit style fixtures

---
+++ packages/stylelint/fixtures/index.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+/* stylelint-disable order/properties-order */
+div {
+  background-color: slategray;
+  box-sizing: border-box;
+  flex: 1 1 auto;
+  font-size: 1.5rem;
+  grid-gap: 16px;
+  order: 1;
+  pointer-events: all;
+  position: relative;
+  transition: opacity 300ms ease;
+  width: 320px;
+}
+</style>
+
+<style lang="scss" scoped>
+div {
+  background-color: slategray;
+  box-sizing: border-box;
+  flex: 1 1 auto;
+  font-size: 1.5rem;
+  grid-gap: 16px;
+  order: 1;
+  pointer-events: all;
+  position: relative;
+  transition: opacity 300ms ease;
+  width: 320px;
+}
+</style>
