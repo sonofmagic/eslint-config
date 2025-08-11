f56e25b1f20c33982a12d6cea819be885cdbcdb8
ice breaker <1324318532@qq.com>
2025-08-11 23:28:13 +0800

    chore: bump version

--- packages/commitlint/src/index.ts
+++ packages/commitlint/src/index.ts
@@ -1,10 +1,20 @@
 import type { UserConfig } from '@commitlint/types'
+import preset from '@commitlint/config-conventional'
+import {
+  RuleConfigCondition,
+  RuleConfigSeverity,
+  TargetCaseType,
+} from '@commitlint/types'
 import { defu } from 'defu'
 
 export function icebreaker(config?: UserConfig): UserConfig {
-  return defu(config, {
-    extends: ['@commitlint/config-conventional'],
-  })
+  return defu<UserConfig, UserConfig[]>(config, preset)
+}
+
+export {
+  RuleConfigCondition,
+  RuleConfigSeverity,
+  TargetCaseType,
 }
 
 export type {
